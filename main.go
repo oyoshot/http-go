feb6c2c3fb0af2f231956835e3821a678e7dbcc6
oyoshot <oyoshot@gmail.com>
2024-02-18 15:20:24 +0900

    Add test

--- main.go
+++ main.go
@@ -5,11 +5,25 @@ import (
 	"net/http"
 )
 
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, go"))
-	})
+	http.HandleFunc("/", NewHandler(helloHandler))
 	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func NewHandler(h HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h(w, r)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello, go\n"))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 }
